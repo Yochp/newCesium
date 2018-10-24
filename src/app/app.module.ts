fcd0c500e72d6520d87f02e8b5b3c0c23c4ef709
yochp <peled770@gmail.com>
2018-10-24 23:25:37 +0300

    install Socket io and import it  ngModule, + insert line in the polyfills file to windows as any (bugfix)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, Injectable } from '@angular/core';
+import { SocketIoModule, SocketIoConfig, Socket} from 'ng-socket-io';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CesiumComponent } from './cesium/cesium.component';
+import { map } from 'rxjs/operators';
+
+
+// io
+
+const config: SocketIoConfig = { url: 'http://localhost:8988', options: {} };
+
+@Injectable()
+class ChatService {
+
+  constructor(private socket: Socket) { }
+
+  sendMessage(msg: string) {
+    this.socket.emit('message', msg);
+  }
+
+  getMessage() {
+    return this.socket
+      .fromEvent<any>('message')
+      .pipe(map(data => data.msg ));
+
+  }
+
+  close() {
+    this.socket.disconnect();
+  }
+}
+
+
+// io-done
+
+
 
 @NgModule({
   declarations: [
@@ -12,9 +44,15 @@ import { CesiumComponent } from './cesium/cesium.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocketIoModule.forRoot(config)
   ],
-  providers: [],
+  providers: [ChatService],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
+
