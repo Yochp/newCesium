fcb6a337f28ef0eb618fd6945fda0c33f47c81ab
yochp <peled770@gmail.com>
2018-10-29 17:19:48 +0200

    create observ in radars to put it in acFor

--- src/app/cesium/cesium.component.ts
+++ src/app/cesium/cesium.component.ts
@@ -1,5 +1,7 @@
-import {Component, OnInit, ElementRef} from '@angular/core';
+import {Component, OnInit, ElementRef, OnDestroy} from '@angular/core';
 import {SocketService} from '../socket.service';
+import {Observable} from 'rxjs';
+import {from, Subject, BehaviorSubject} from 'rxjs';
 
 @Component({
   selector: 'app-cesium',
@@ -9,18 +11,61 @@ import {SocketService} from '../socket.service';
 })
 
 
-export class CesiumComponent implements OnInit {
+export class CesiumComponent implements OnInit, OnDestroy {
+  connection;
+  // radars;
+
+  radars$: Observable<any>;
+  // private radarsSubject: Subject<any>;
 
   constructor(private el: ElementRef, private socketService: SocketService) {
+    //
+    // this.radarsSubject = new Subject();
+    // this.radars$ = this.radarsSubject.asObservable();
+    // this.radars$ = this.radars$.Observable;
+
+    this.radars$ = new Observable<any>();
+
   }
 
 
+  // console.log('call to socket init from ngOnInit ');
   ngOnInit() {
-      this.socketService.socketInit();
-      console.log('call to socket init from ngOnInit');
-    }
+    // this.socketService.socketInit();
+    // this.connection = this.socketService.socketInit().subscribe(radarsData => {
+    //   this.radars$ = radarsData;
+    //   console.log('call to socket init from ngOnInit obzerbevel');
+    //   console.log(this.radars$);
+    // });
+    //
+    // console.log('call to socket init from ngOnInit ');
+// 111111
+//     this.connection = this.socketService.socketInit().subscribe(radarsData => {
+//       this.radarsSubject.next(radarsData)
+//       // this.radars$ = Observable.create(radarsData);
+//       console.log('call to socket init from ngOnInit obzerbevel');
+//       console.log(this.radarsSubject);
+//     });
 
+    this.connection = this.socketService.socketInit().subscribe(radarsData => {
+      // this.radars$ = radarsData;
+      console.log(radarsData);
 
+      this.radars$ = new BehaviorSubject(radarsData);
+
+      console.log('call to socket init from ngOnInit obzerbevel');
+      console.log(this.radars$);
+    });
+
+    // this.radars$ = this.socketService.socketInit();
   }
+
+
+  ngOnDestroy() {
+    this.connection.unsubscribe();
+  }
+}
+
+
 
 
--- src/app/socket.service.ts
+++ src/app/socket.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import * as io from 'socket.io-client';
-import {Observable} from 'rxjs';
+import {Subject} from 'rxjs/Subject';
+import {Observable} from 'rxjs/Observable';
 
 
 @Injectable({
@@ -8,15 +9,44 @@ import {Observable} from 'rxjs';
 })
 
 export class SocketService {
+  // radars;
+  //
+  // // radarsPromise;
+  //
+  //
+  // socketInit() {
+  //   const socket = io('http://localhost:3000');
+  //   socket.on('get_radars', (data) => {
+  //     this.radars = data;
+  //     console.log(data);
+  //     console.log(this.radars);
+  //
+  //   });
+  //
+  // }
 
+  private url = 'http://localhost:3000';
+  private socket;
+
+  // sendMessage(message) {
+  //   this.socket.emit('add-message', message);
+  // }
 
   socketInit() {
-    const socket = io('http://localhost:3000');
-    socket.on('hello', (data) => console.log(data));
-
+    const observable = new Observable(observer => {
+      this.socket = io(this.url);
+      this.socket.on('get_radars', (data) => {
+        observer.next(data);
+      });
+      // return () => {
+      //   this.socket.disconnect();
+      // };
+    });
+    return observable;
   }
-
 
 }
 
+
 console.log('socketClient on');
+
