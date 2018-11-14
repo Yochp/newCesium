99c30dad3d44b8fea9336118f23ffd016eaf7683
yochp <peled770@gmail.com>
2018-11-14 15:21:26 +0200

    crate object for rhe radars and show it on the map (no ngrx) beggint to add points with ngrx

--- src/app/socket.service.ts
+++ src/app/socket.service.ts
@@ -2,11 +2,16 @@ import {Injectable} from '@angular/core';
 import * as io from 'socket.io-client';
 import {Subject} from 'rxjs/Subject';
 import {Observable} from 'rxjs/Observable';
-import { AcNotification, ActionType } from 'angular-cesium';
+import {Subscriber} from 'rxjs/Subscriber';
+import {AcNotification, ActionType} from 'angular-cesium';
 
 // store
-import { Store } from '@ngrx/store';
-// import { AddRadars } from './store/actions/radars.action';
+import {Store} from '@ngrx/store';
+import {RadarPositoin} from './models/radar.model';
+import {RadarPositoinState} from './store/state/state';
+import {AddRadars} from './store/actions/radars.action';
+import {AddPoints} from './store/actions/points.action';
+
 
 // export class WebSocketService {
 //   private socket;
@@ -27,7 +32,6 @@ import { Store } from '@ngrx/store';
 // }
 
 
-
 @Injectable({
   providedIn: 'root'
 })
@@ -36,36 +40,93 @@ export class SocketService {
   private url = 'http://localhost:3000';
   private socket;
 
-  // constructor(private store: Store<any>) { }
-  constructor() { }
+  // radarP_Store: Observable<RadarPositoin[]>;
 
 
-  socketInit() {
-    const observable = new Observable<AcNotification>(observer => {
-      this.socket = io(this.url);
-      this.socket.on('get_radars', (data) => {
-        // ngrx
-        //       this.store.dispatch(new AddRadars(data));
-        observer.next(data);
-      });
-      // return () => {
-      //   this.socket.disconnect();
+  // constructor(private store: Store<any>) {
+  // this.radarP_Store = store.select('radarsP');
+
+
+  // }
+  constructor(private store: Store<any>) {
+    this.socket = io(this.url);
+    // this.socket.on('get_randomPoint', (data) => {
+    //       this.store.dispatch(new AddPoints({data}));
+    //       console.log('sdsdsds' + data);
+    //     });
+
+    this.socket.on('get_randomPoint', (data: any) => {
+      this.store.dispatch(new AddPoints({data}));
     });
-    return observable;
   }
+
+  //
+  // socketInit() {
+  //   const observable = new Observable<AcNotification>(observer => {
+  //     this.socket = io(this.url);
+  //     this.socket.on('get_radars', (data) => {
+  //       // ngrx
+  //       //       this.store.dispatch(new AddRadars(data));
+  //       observer.next(data);
+  //     });
+  //     // return () => {
+  //     //   this.socket.disconnect();
+  //   });
+  //   return observable;
+  // }
 
 
   getRadarsPosition(): Observable<any> {
     const observable = new Observable(observer => {
-      this.socket = io(this.url);
       this.socket.on('get_radarsPosition', (data) => {
+        this.store.dispatch(new AddRadars({data}));
         observer.next(data);
       });
     });
     return observable;
   }
-}
 
+
+  // socket.on('get_randomPoint', (data) => {
+  //       // this.store.dispatch(new AddRadars({data}));
+  //       console.log('sdsdsds' + data);
+  //       observer.next(data);
+  //     });
+  //
+  //
+  // getRadarsPoint(): Observable<any> {
+  //   const observable = new Observable(observer => {
+  //     this.socket.on('get_randomPoint', (data) => {
+  //       this.store.dispatch(new AddPoints({data}));
+  //       // console.log('sdsdsds' + data);
+  //       observer.next(data);
+  //     });
+  //   });
+  //   return observable;
+  // }
+
+
+  // getPoint() {
+  //   return Observable.create((observer: Subscriber<any>) => {
+  //     this.socket.on('get_randomPoint', (data: any) => {
+  //       this.store.dispatch(new AddPoints({data}));
+  //       // data.forEach(
+  //       //   (acNotification: any) => {
+  //       //     let action;
+  //       //     if (acNotification.action === 'ADD_OR_UPDATE') {
+  //       //       action = ActionType.ADD_UPDATE;
+  //       //     }
+  //       //     else if (acNotification.action === 'DELETE') {
+  //       //       action = ActionType.DELETE;
+  //       //     }
+  //       //     acNotification.actionType = action;
+  //       //     acNotification.entity = createPentity(acNotification.entity, data);
+  //       //     observer.next(acNotification);
+  //       //   });
+  //     });
+  //   });
+  // }
+}
 
 console.log('socketClient on');
 
